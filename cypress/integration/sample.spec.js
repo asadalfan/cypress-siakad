d52b540b7dcea9655d816408b029634caee53e86
Your Name <you@example.com>
2021-08-07 15:29:32 +0700

    belajar cypress

---
+++ cypress/integration/sample.spec.js
@@ -0,0 +1,65 @@
+/// <reference types="cypress" />
+
+describe('User melakukan proses login pada SIAKAD Polinema', () => {
+    it('User memasukkan username berupa NIM dan password yang terdaftar', () => {
+      cy.visit('http://siakad.polinema.ac.id/')
+      cy.get('#username').type('1631710039')
+      cy.get('#password').type('sempakbocor1234')
+      cy.get('.btn-success').click()
+    })
+  
+    it('User memasukkan username berupa NIM dan password yang tidak terdaftar', () => {
+      cy.visit('http://siakad.polinema.ac.id/')
+      cy.get('#username').type('asad')
+      cy.get('#password').type('asad')
+      cy.get('.btn-success').click()
+      cy.contains('Username / Password Salah')
+    })
+  
+    it('User memasukkan nim dan tidak mengisi password', () => {
+      cy.visit('http://siakad.polinema.ac.id/')
+      cy.get('#username').type('1631710039')
+      cy.get('.btn-success').click()
+    })
+  
+    it('User memasukkan password dan tidak mengisi nim', () => {
+      cy.visit('http://siakad.polinema.ac.id/')
+      cy.get('#password').type('1631710039')
+      cy.get('.btn-success').click()
+    })
+  
+  
+    it('User mengklik tombol lupa password', () => {
+      cy.visit('http://siakad.polinema.ac.id/')
+      
+      cy.contains('Lupa Password?').click()
+      // Should be on a new URL which includes '/commands/actions'
+      cy.url().should('include', 'https://helpakademik.polinema.ac.id/ajukan-pertanyaan')
+  
+    })
+  
+    // id lupa password
+    // forget-password
+    
+    // it('Gets, types and asserts', () => {
+    //   cy.visit('https://example.cypress.io')
+  
+    //   cy.contains('type').click()
+  
+    //   // Should be on a new URL which includes '/commands/actions'
+    //   cy.url().should('include', '/commands/actions')
+  
+    //   // Get an input, type into it and verify that the value has been updated
+    //   cy.get('.action-email')
+    //     .type('fake@email.com')
+    //     .should('have.value', 'fake@email.com')
+    // })
+  
+  
+  
+  
+  
+  
+    
+  
+  })
